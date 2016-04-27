bbe1fa432c07b158f48d5449f9eeac2a25b56d7d
okunoryo <okuno.ryo.411@gmail.com>
2016-04-27 23:06:17 +0900

    GPS read ok

---
+++ logging.py
@@ -0,0 +1,18 @@
+import serial
+from datetime import datetime
+
+if __name__ == "__main__":
+    ser = serial.Serial("/dev/tty.usbserial",4800,timeout=10)
+
+    def convert(lat,lng):
+        lat = float(lat) / 100
+        lng = float(lng) / 100
+        return lat,lng
+   
+    while True:
+        line = ser.readline()
+        if line.startswith("$GPGGA"):
+            data = line.split(",")
+            print "raw_data: "+line
+            lat,lng = convert(data[2],data[4])
+            print lat , "," , lng , "\n"
